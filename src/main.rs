f93f5c3d29aadcd9611a6e90c3d2d3bd44c30d34
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-02-01 10:28:21 +0530

    creating struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        email: String::from("cornell@deg.com"),
+        username: String::from("cornell"),
+        sign_in_count: 1,
+        active: true,
+    };
+
+    let name = user1.username;
+
+    user1.username = String::from("cornell_1");
+
+
 }
