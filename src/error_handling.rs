ceeb2b1202b6c3124017934f698398f4e788639d
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-02-03 00:04:03 +0530

    file structuring

---
+++ src/error_handling.rs
@@ -0,0 +1,85 @@
+mod error_handling {
+    use std::{fs::{self, File}, io::{self, ErrorKind}};
+    use core::panic;
+
+    pub fn test_error(){
+    
+    }
+    
+    pub fn test_int_error(){
+        let i = 22;
+    
+        if i == 22 {
+            panic!("Do not pass 22");
+        }
+    }
+    
+    pub fn test_file_error(){
+        
+        // The error of Result<T,E> type
+        let f = File::open("hello.txt");
+    
+    
+        // simplest way of handling the error
+        // let f = File::open("hello.txt").expect("Failed to open the file");
+    
+        // ungraceful handling
+        // let f = match f {
+        //     Ok(file) => file,
+        //     Err(error) => panic!("File ERROR: {:#?}",error)
+        // };
+    
+        // handling the errors gracefully (but code written without closures)
+        // {
+        //     let f = match f {
+        //         Ok(file) => file,
+        //         Err(error) => match error.kind() {
+        //             ErrorKind::NotFound => match File::create("hello.txt") {
+        //                 Ok(fc) => fc,
+        //                 Err(e) => panic!("problem creating file: {:#?}",e)
+        //             },
+        //             all_other_error_kind => {
+        //                 panic!("Other Error occured while opening: {:#?}",all_other_error_kind)
+        //             }
+        //         }
+        //     };
+        // }
+    
+        // handling the error with closure
+        {
+            let f = File::open("hello.txt").unwrap_or_else( |err | {
+                if err.kind() == ErrorKind::NotFound {
+                    File::create("hello.txt").unwrap_or_else(|err | {
+                        panic!("Problem creating file: {:#?}",err)
+                    })
+                }else {
+                    panic!("Problem opening file: {:#?}",err)
+                }
+            });
+        }
+        
+    }
+    
+    pub fn test_file_error_propagation() -> Result<String, io::Error>{
+        // adding ? which is similar functionality as unwrap_or_else() function
+        // {
+        //     let mut f = File::open("hello.txt")?;
+    
+        //     let mut s = String::new();
+    
+        //     f.read_to_string(&mut s)?;
+    
+        //     // return the result if the file is present
+        //     Ok(s)
+        // }
+    
+        // shortest format of code which returns the specified type
+        fs::read_to_string("hello.txt")
+        
+    
+        // but the ? operator can not be used in main function without customizing the main function by using Unit and Boxing the type
+    
+    }
+}
+
+pub use self::error_handling::*;
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,12 @@
-use core::panic;
-use std::{fs::{self, File}, io::{self, ErrorKind, Read}};
+use error_handling::{test_file_error, test_file_error_propagation};
 
 mod rectangle;
 mod enums;
 mod collections;
-
-
-pub fn test_error(){
-    
-}
-
-pub fn test_int_error(){
-    let i = 22;
-
-    if i == 22 {
-        panic!("Do not pass 22");
-    }
-}
-
-pub fn test_file_error(){
-    
-    // The error of Result<T,E> type
-    let f = File::open("hello.txt");
-
-
-    // simplest way of handling the error
-    // let f = File::open("hello.txt").expect("Failed to open the file");
-
-    // ungraceful handling
-    // let f = match f {
-    //     Ok(file) => file,
-    //     Err(error) => panic!("File ERROR: {:#?}",error)
-    // };
-
-    // handling the errors gracefully (but code written without closures)
-    // {
-    //     let f = match f {
-    //         Ok(file) => file,
-    //         Err(error) => match error.kind() {
-    //             ErrorKind::NotFound => match File::create("hello.txt") {
-    //                 Ok(fc) => fc,
-    //                 Err(e) => panic!("problem creating file: {:#?}",e)
-    //             },
-    //             all_other_error_kind => {
-    //                 panic!("Other Error occured while opening: {:#?}",all_other_error_kind)
-    //             }
-    //         }
-    //     };
-    // }
-
-    // handling the error with closure
-    {
-        let f = File::open("hello.txt").unwrap_or_else( |err | {
-            if err.kind() == ErrorKind::NotFound {
-                File::create("hello.txt").unwrap_or_else(|err | {
-                    panic!("Problem creating file: {:#?}",err)
-                })
-            }else {
-                panic!("Problem opening file: {:#?}",err)
-            }
-        });
-    }
-    
-}
-
-pub fn test_file_error_propagation() -> Result<String, io::Error>{
-    // adding ? which is similar functionality as unwrap_or_else() function
-    // {
-    //     let mut f = File::open("hello.txt")?;
-
-    //     let mut s = String::new();
-
-    //     f.read_to_string(&mut s)?;
-
-    //     // return the result if the file is present
-    //     Ok(s)
-    // }
-
-    // shortest format of code which returns the specified type
-    fs::read_to_string("hello.txt")
-    
-
-    // but the ? operator can not be used in main function without customizing the main function by using Unit and Boxing the type
-
-}
+mod error_handling;
 
 fn main() {
+    // test_rectangle();
+    // let _ = test_file_error_propagation();
     test_file_error();
 }
