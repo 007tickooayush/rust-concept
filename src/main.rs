cfe7abfbc384588c4e115595f62f699fe83119a4
007tickooayush <ayush.tickoo292000@gmail.com>
2024-02-29 20:35:09 +0530

    concurrency+thread communication running

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use smart_pointers_interior_mutability::smart_pointers_refCell::{test_with_ref, 
 use smart_pointers_reference_counted::smart_pointers_rc::{test_reference_counted, test_without_rc};
 use traits::traits_mod::{test_traits,notify,notify_2};
 use types::types_mod::{get_largest, test_types};
+use crate::concurrency_messages::concurrency_msg::{test_mpsc, test_multiple_tx};
 use crate::concurrency_threads::concurrency::{test_thread_main_vs_spawn_thread_1, test_thread_main_vs_spawn_thread_2, test_thread_main_vs_spawn_thread_3, test_thread_move};
 use crate::smart_pointers_interior_mem_safe::interior_mutability_mem_safe::test_stack_interior;
 
@@ -29,9 +30,12 @@ mod smart_pointers_reference_counted;
 mod smart_pointers_interior_mutability;
 mod smart_pointers_interior_mem_safe;
 mod concurrency_threads;
+mod concurrency_messages;
 
 fn main() {
-    test_thread_move();
+    test_multiple_tx();
+    // test_mpsc();
+    // test_thread_move();
     // test_thread_main_vs_spawn_thread_2();
     // test_stack_interior();
     // test_without_ref();
