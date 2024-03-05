4b7e79f0b68fac3cb9f74bb39eb24c84ea6b9a67
007tickooayush <ayush.tickoo292000@gmail.com>
2024-03-05 11:12:11 +0530

    inheritance mark 3 + test cases

--- src/inheritance.rs
+++ src/inheritance.rs
@@ -11,7 +11,7 @@ pub mod inheritance_demo {
     pub trait SubtractFrom<T> {
         type Item;
 
-        fn subtract_from(&self) ;
+        fn subtract_from(&self) -> Result<T, Self::Item> ;
     }
 
     pub struct AddI32<Nu> {
@@ -25,8 +25,13 @@ pub mod inheritance_demo {
     impl SubtractFrom<i32> for SubtractI32<i32> {
         type Item = i32;
 
-        fn subtract_from(&self) {
-            todo!()
+        fn subtract_from(&self) -> Result<i32, Self::Item> {
+            let first = self.numbers[0];
+            let mut sum =0;
+            for num in &self.numbers[1..] {
+                sum += num;
+            }
+            Ok(first - sum)
         }
     }
     impl AddNumbers<i32> for AddI32<i32> {
@@ -50,20 +55,11 @@ pub mod inheritance_demo {
         assert_eq!(result, 15);
     }
 
-}
+    #[test]
+    pub fn test_subtraction() {
+        let sub = SubtractI32 { numbers: vec![10,5,3] };
+        let result = sub.subtract_from().unwrap().checked_abs().unwrap();
+        assert_eq!(result, 2);
+    }
 
-// correct impl for the AddNumbers trait provide <i32>
-// pub mod inheritance_demo {
-//     pub struct Number<T> {
-//         num: T
-//     }
-//
-//     impl Number<i32> {
-//
-//         pub fn new(num: i32) -> Number<i32> {
-//             Number { num }
-//         }
-//     }
-//
-//
-// }
+}
